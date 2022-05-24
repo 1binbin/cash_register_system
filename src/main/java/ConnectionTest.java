c523472c1a6b504a6902e14984b832720803a807
hongxiaobin <2812181610@qq.com>
2022-05-24 15:26:37 +0800

    2812181610@qq.com

---
+++ src/main/java/ConnectionTest.java
@@ -0,0 +1,25 @@
+import com.example.cash_register_system.Loginin;
+
+import java.sql.Connection;
+
+/**
+ * @Author hongxiaobin
+ * @Time 2022/5/24-15:23
+ */
+public class ConnectionTest {
+    public static void main(String[] args) {
+        try {
+            Loginin loginin = new Loginin();
+            loginin.init();
+            Connection connection = Loginin.connection;
+            System.out.println(connection);
+            if (connection != null) {
+                System.out.println("数据库连接正常");
+            } else {
+                System.out.println("数据库连接异常，请检查");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
